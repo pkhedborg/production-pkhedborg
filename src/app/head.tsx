c3ccfa78c84dfb5299993434a4fba53b00633c2c
Ohedborg <oliver.hedborg@gmail.com>
2025-05-29 12:13:25 +0100

    date update

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Your Site Title</title>
+      <meta name="description" content="Your site description" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+} 
